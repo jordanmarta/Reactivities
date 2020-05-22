afafd28ed626dcee9b3610c86654d32235ae7574
jordan <jordan@gmail.com>
2020-05-21 21:59:56 -0300

    commit-apos-formatacao

--- frontend/src/features/activities/form/ActivityForm.tsx
+++ frontend/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FormEvent, useContext, useEffect } from 'react';
-import { Segment, Form, Button } from 'semantic-ui-react';
+import { Segment, Form, Button, Grid } from 'semantic-ui-react';
 import { IActivity } from '../../../app/models/activity';
 import { v4 as uuid } from 'uuid';
 import ActivityStore from '../../../app/stores/activityStore';
@@ -77,7 +77,8 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
   };
 
   return (
-    // Sem o clearing os botões não ficam dentro do container
+    <Grid>
+      <Grid.Column width={10}>
     <Segment clearing>
       <Form onSubmit={handleSubmit}>
         <Form.Input
@@ -134,6 +135,8 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
         />
       </Form>
     </Segment>
+      </Grid.Column>
+    </Grid>
   );
 };
 
